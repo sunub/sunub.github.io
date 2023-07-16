389a14024ab84f39dd319938d3df43b10e5dfec3
sunub <bsc5672@gmail.com>
2023-07-16 23:56:13 +0900

    Adding some animation

--- src/app/[category]/[slug]/layout.tsx
+++ src/app/[category]/[slug]/layout.tsx
@@ -1,10 +1,11 @@
 import React, { ReactNode } from 'react';
 import MainBirdIcon from '@/components/icon/MainBridIcon';
+import FlyingBird from '@/components/icon/FlyingBird';
 
 export default function Layout({ children }: { children: ReactNode }) {
     return (
         <div>
-            <MainBirdIcon />
+            <FlyingBird />
             {children}
         </div>
     )
