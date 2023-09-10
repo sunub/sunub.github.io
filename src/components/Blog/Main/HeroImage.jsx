31bbbb8091bc6e7fa8d2db45b5ef68a7365c10fd
sunub <bsc5672@gmail.com>
2023-09-10 23:36:38 +0900

    fix: Fix the blog design

--- src/components/Blog/Main/HeroImage.jsx
+++ src/components/Blog/Main/HeroImage.jsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import styled from "styled-components";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
 import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
 import * as THREE from "three";
 import { baseURL } from "@/utils/getBaseUrl";
@@ -22,14 +22,62 @@ function loadSVG(url) {
 		let renderOrder = 0;
 
 		for (const path of data.paths) {
-			console.log(path);
+			const fillColor = path.userData?.style.fill;
+
+			if (fillColor !== undefined && fillColor !== "none") {
+				const material = new THREE.MeshBasicMaterial({
+					color: new THREE.Color().setStyle(fillColor),
+					opacity: path.userData?.style.fillOpacity,
+					transparent: true,
+					side: THREE.DoubleSide,
+					depthWrite: false,
+				});
+
+				const shapes = SVGLoader.createShapes(path);
+
+				for (const shape of shapes) {
+					const geometry = new THREE.ShapeGeometry(shape);
+					const mesh = new THREE.Mesh(geometry, material);
+					mesh.renderOrder = renderOrder++;
+
+					group.add(mesh);
+				}
+			}
+
+			const strokeColor = path.userData.style.stroke;
+			if (strokeColor !== undefined && strokeColor !== "none") {
+				const material = new THREE.MeshBasicMaterial({
+					color: new THREE.Color().setStyle(strokeColor),
+					opacity: path.userData.style.strokeOpacity,
+					transparent: true,
+					side: THREE.DoubleSide,
+					depthWrite: false,
+				});
+
+				for (const subPath of path.subPaths) {
+					const geometry = SVGLoader.pointsToStroke(
+						subPath.getPoints(),
+						path.userData.style
+					);
+
+					if (geometry) {
+						const mesh = new THREE.Mesh(geometry, material);
+						mesh.renderOrder = renderOrder++;
+						group.add(mesh);
+					}
+				}
+			}
 		}
 	});
 }
 
-export default function HeroImage() {
-	loadSVG(url);
+function Model() {
+	const result = useLoader(SVGLoader, url);
 
+	return <primitive object={result.scene} />;
+}
+
+export default function HeroImage() {
 	return (
 		<>
 			<Canvas
@@ -38,7 +86,9 @@ export default function HeroImage() {
 					width: "100dvw",
 					height: "65dvh",
 				}}
-			></Canvas>
+			>
+				<Model />
+			</Canvas>
 		</>
 	);
 }
---
+++ src/components/WebGPU/canvas.jsx
@@ -0,0 +1,8 @@
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+
+function BaseCanvas() {
+	const group = React.useRef();
+
+	return <Canvas></Canvas>;
+}
