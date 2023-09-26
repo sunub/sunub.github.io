6f45ee4d386d1a6ab7a7f5f93dfdbfe3af6b8721
sunub <bsc5672@gmail.com>
2023-09-27 00:49:56 +0900

    fix: Fix the same error

--- src/utils/post/Post.constant.ts
+++ src/utils/post/Post.constant.ts
@@ -1,9 +1,6 @@
 import fs from "fs";
 
-export const POST_ROOT_PATH =
-	process.env.NODE_ENV === "development"
-		? `${process.env.DEV_PORT}/post`
-		: `${process.env.DEPLOY_PORT}/post`;
+export const POST_ROOT_PATH = `posts/`;
 
 export const POST_CATEGORY: ReadonlyArray<string> = fs.readdirSync(
 	POST_ROOT_PATH,
