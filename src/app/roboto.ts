48c207783db777b2cf4efc36742805242504d905
sunub <bsc5672@gmail.com>
2023-11-26 23:00:24 +0900

    feat : sitemap.xml과 roboto.txt 추가
    nextjs의 sitemap.ts, roboto.ts를 이용하여 sitemap.xml과 rototo.txt를 추가해주었다.

---
+++ src/app/roboto.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+    },
+    sitemap: "https://sunub.vercel.app/sitemap.xml",
+    host: "https://sunub.vercel.app/",
+  };
+}
