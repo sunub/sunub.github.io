13fa70f20ad45b48376ba152d9a24f68df3d9f90
sunub <bsc5672@gmail.com>
2023-12-25 23:53:12 +0900

    feat : 태그를 통해 분류가 가능하게끔 코드 작성
    작성한 블로그 포스트에 태그를 추가하여 어떤 내용을 다룬 글인지 파악할 수 있게끔 태그를 통해 구분할 수 있게끔 코드를 작성하였다.

--- src/utils/Post.js
+++ src/utils/Post.js
@@ -1,60 +1,78 @@
 import { getLocalTagFiles, categorizePostByCategory } from "./Post.helper";
 
 class Post {
-	constructor() {
-		this._localPostFilesData = getLocalTagFiles();
-		this._categorizedFrontmatters = categorizePostByCategory(
-			this._localPostFilesData
-		);
-		this._localPostContents = this.getLocalPostContents();
-	}
+  constructor() {
+    this._localPostFilesData = getLocalTagFiles();
+    this._categorizedFrontmatters = categorizePostByCategory(
+      this._localPostFilesData,
+    );
+    this._localPostContents = this.getLocalPostContents();
+  }
 
-	get allTagPost() {
-		return this._localPostFilesData;
-	}
+  get allTagPost() {
+    return this._localPostFilesData;
+  }
 
-	get contents() {
-		return this._localPostContents;
-	}
+  get contents() {
+    return this._localPostContents;
+  }
 
-	get categories() {
-		return Object.keys(this._localPostFilesData);
-	}
+  get categories() {
+    return Object.keys(this._localPostFilesData);
+  }
 
-	get frontMatters() {
-		return this._categorizedFrontmatters;
-	}
+  get frontMatters() {
+    return this._categorizedFrontmatters;
+  }
 
-	getSpecificFrontmatter(category, slug) {
-		const frontMatters = this.frontMatters[category] ?? [];
+  get tags() {
+    const allTags = new Map();
+    const allFrontMatters = this.frontMatters;
 
-		for (const frontMatter of frontMatters) {
-			if (frontMatter.slug === slug) {
-				return frontMatter;
-			}
-		}
-		throw new Error(
-			`FrontMatter에서 ${slug}와 관련된 slug를 발견할 수 없습니다.`
-		);
-	}
+    for (const frontmatter of allFrontMatters.all) {
+      let { tags } = frontmatter;
 
-	getLocalPostContents() {
-		const result = {};
-		const localPostData = Object.values(this._localPostFilesData);
+      for (const tag of tags) {
+        if (tag === "") continue;
+        allTags.has(tag)
+          ? allTags.set(tag, allTags.get(tag) + 1)
+          : allTags.set(tag, 1);
+      }
+    }
 
-		for (const data of localPostData) {
-			for (const info of data) {
-				const key = info.description.slug;
-				result[key] = info.content;
-			}
-		}
+    return new Map([...allTags.entries()].sort((a, b) => b[1] - a[1]));
+  }
 
-		return result;
-	}
+  getSpecificFrontmatter(category, slug) {
+    const frontMatters = this.frontMatters[category] ?? [];
 
-	getCategorizedPost(key) {
-		return this._localPostFilesData[key];
-	}
+    for (const frontMatter of frontMatters) {
+      if (frontMatter.slug === slug) {
+        return frontMatter;
+      }
+    }
+    throw new Error(
+      `FrontMatter에서 ${slug}와 관련된 slug를 발견할 수 없습니다.`,
+    );
+  }
+
+  getLocalPostContents() {
+    const result = {};
+    const localPostData = Object.values(this._localPostFilesData);
+
+    for (const data of localPostData) {
+      for (const info of data) {
+        const key = info.description.slug;
+        result[key] = info.content;
+      }
+    }
+
+    return result;
+  }
+
+  getCategorizedPost(key) {
+    return this._localPostFilesData[key];
+  }
 }
 
 export default Post;
