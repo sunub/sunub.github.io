eea7bd1359d84c6e5b47560575fef1a5c5d4c946
sunub <bsc5672@gmail.com>
2023-12-25 23:51:23 +0900

    fix : 블로그 포스트 본문에 요약문을 작성할 수 있게끔 코드 작성

---
+++ src/components/v2/Main/NewestPost/NewestPost.style.ts
@@ -0,0 +1,18 @@
+"use client";
+
+import styled from "styled-components";
+
+export const RootWrapper = styled.div`
+  grid-area: newest;
+  font-family: "Wotfard", "NanumSquareNeo", serif;
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const Title = styled.h1`
+  font-size: 2rem;
+`;
