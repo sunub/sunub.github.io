23c8b5c79bd934d0ca66f59843679f6e31caf035
sunub <bsc5672@gmail.com>
2023-12-29 23:41:43 +0900

    refactor : 사용자의 Color Prefer을 설정하고 변경하는 방식 변경
    기존의 방법은 Context와 localstorage를 이용하여 사용자의 color prefer을 설정하고 변경하였지만 이를  cookie를 이용하여 설정하는 방법으로 변경하였다.

--- src/components/Theme/InitTheme/InitTheme.jsx
+++ src/components/Theme/InitTheme/InitTheme.jsx
@@ -1,98 +1,81 @@
 import React from "react";
 
 function setColorsByTheme() {
-  const COLORS = {
-    text: {
-      light: `oklch(32.31% 0.0262 34.69 / 90%)`,
-      dark: `oklch(97.14% 0.011 31.07)`,
-    },
-    background: {
-      light: `oklch(97.14% 0.011 31.07)`,
-      dark: `oklch(23.93% 0 0)`,
-    },
-    primary: {
-      light: `oklch(100% 0 0 / 0.8)`,
-      dark: `oklch(34% 0.019 229.64)`,
-    },
-    highlightColor: {
-      light: `oklch(70.8% 0.165 32.85)`,
-      dark: `oklch(90.97% 0.046 246.35)`,
-    },
-    header: {
-      light: `oklch(94.51% 0.022 31.78)`,
-      dark: `oklch(30.4% 0.005 247.98)`,
-    },
-    icon: {
-      light: `oklch(61.8% 0.027 30.58)`,
-      dark: `oklch(97.14% 0.011 31.07)`,
-    },
-    elevation: {
-      light: "oklch(100% 0 0)",
-      dark: "oklch(45.42% 0.007 239.95)",
-    },
-    bird: {
-      light: "oklch(73.44% 0.152 21.47)",
-      dark: "oklch(90.21% 0.055771670330652126 300.11937740281473)",
-    },
-    thumb: {
-      light: "oklch(92.54% 0.01 32.52)",
-      dark: "oklch(92.17% 0.039 291.94)",
-    },
-    thumbBackground: {
-      light: "oklch(53.74% 0.029 30.1)",
-      dark: "oklch(45.42% 0.007 239.95)",
-    },
+  const LIGHT_COLORS = {
+    "--color-navlink": `oklch(42.14% 0.08 29.36)`,
+    "--color-text": `oklch(21.08% 0.055 34.69)`,
+    "--color-background": `oklch(97.14% 0.011 31.07)`,
+    "--color-primary": `oklch(100% 0 0 / 0.8)`,
+    "--color-highlight": `oklch(70.8% 0.165 32.85)`,
+    "--color-header": `oklch(90.8% 0.046 29.64)`,
+    "--color-icon": `oklch(61.8% 0.027 30.58)`,
+    "--color-elevation": `oklch(100% 0 0)`,
+    "--color-bird": `oklch(73.44% 0.152 21.47)`,
+    "--color-thumb": `oklch(92.54% 0.01 32.52)`,
+    "--color-thumb-background": `oklch(53.74% 0.029 30.1)`,
+  };
+
+  const DARK_COLORS = {
+    "--color-navlink": `oklch(90.21% 0.055771670330652126 300.11937740281473)`,
+    "--color-text": `oklch(100% 0 31.08)`,
+    "--color-background": `oklch(23.93% 0 0)`,
+    "--color-primary": `oklch(34% 0.019 229.64)`,
+    "--color-highlight": `oklch(64.86% 0.181 249.54)`,
+    "--color-header": `oklch(30.4% 0.005 247.98)`,
+    "--color-icon": `oklch(97.14% 0.011 31.07)`,
+    "--color-elevation": `oklch(45.42% 0.007 239.95)`,
+    "--color-bird": `oklch(90.21% 0.055771670330652126 300.11937740281473)`,
+    "--color-thumb": `oklch(92.17% 0.039 291.94)`,
+    "--color-thumb-background": `oklch(45.42% 0.007 239.95)`,
   };
 
   function getInitialColorMode() {
-    const persistedColorPreference =
-      window.localStorage.getItem("theme-preference");
-    const hasPersistedPreference = typeof persistedColorPreference === "string";
+    const persistedCookies = document.cookie.match(/color-theme=([^;]+)/)?.[1];
+    const hasPersistedPreference = typeof persistedCookies === "string";
 
     if (hasPersistedPreference) {
-      return persistedColorPreference;
+      return persistedCookies;
     }
 
     const mql = window.matchMedia("(prefers-color-scheme: dark)");
     const hasMediaQueryPreference = typeof mql.matches === "boolean";
     if (hasMediaQueryPreference) {
       if (mql.matches) {
-        setLocalStorage("dark");
+        setCookies("dark");
         return "dark";
       }
-      setLocalStorage("light");
+      setCookies("light");
       return "light";
     }
 
     return "light";
   }
 
-  function setLocalStorage(data) {
-    window.localStorage.setItem("theme-preference", data);
+  function setCookies(data) {
+    document.cookie = `color-theme=${data};path=/`;
   }
 
   const colorMode = getInitialColorMode();
 
-  const root = document.firstElementChild;
+  const root = document.documentElement;
+  const COLORS = colorMode === "light" ? LIGHT_COLORS : DARK_COLORS;
 
-  root.setAttribute("data-theme", colorMode);
-  Object.entries(COLORS).forEach(([name, colorByTheme]) => {
-    const cssVar = `--color-`;
-    const cssVarName = cssVar + name;
-    root.style.setProperty(cssVarName, colorByTheme[colorMode]);
+  root.setAttribute("data-color-theme", colorMode);
+  Object.entries(COLORS).forEach(([key, value]) => {
+    root.style.setProperty(key, value);
   });
 
   window
     .matchMedia("(prefers-color-scheme: dark)")
     .addEventListener("change", ({ matches: isDark }) => {
       const currColorMode = isDark === true ? "dark" : "light";
-      Object.entries(COLORS).forEach(([name, colorByTheme]) => {
-        const cssVar = "--color-";
-        const cssVarName = cssVar + name;
-        document.firstElementChild.style.setProperty(
-          cssVarName,
-          colorByTheme[currColorMode],
-        );
+      const currPreference = document.cookie.match(/color-theme=([^;]+)/)?.[1];
+      if (currColorMode !== currPreference) {
+        document.cookie = `color-theme=${currColorMode};path=/`;
+      }
+      const COLORS = currColorMode === "light" ? LIGHT_COLORS : DARK_COLORS;
+      Object.entries(COLORS).forEach(([key, value]) => {
+        document.documentElement.style.setProperty(key, value);
       });
     });
 }
--- src/components/Theme/Provider/ThemeProvider.tsx
+++ src/components/Theme/Provider/ThemeProvider.tsx
@@ -5,39 +5,39 @@ import { COLORS, COLOR_MODE_KEY } from "@/constants/constants";
 export const ThemeContext = React.createContext<any>({});
 
 export default function ThemeProvider({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	const [colorMode, rawSetColorMode] = React.useState<string | null>(null);
+  const [colorMode, rawSetColorMode] = React.useState<string | null>(null);
 
-	React.useEffect(() => {
-		const root = document.documentElement;
-		const initColorValue = root.getAttribute("data-theme");
-		rawSetColorMode(initColorValue);
-	}, []);
+  React.useEffect(() => {
+    const root = document.documentElement;
+    const initColorValue = root.getAttribute("data-theme");
+    rawSetColorMode(initColorValue);
+  }, []);
 
-	const contextValue = React.useMemo(() => {
-		const setColorMode = (newValue: "dark" | "light") => {
-			const root = window.document.documentElement;
-			root.setAttribute("data-theme", newValue);
+  const contextValue = React.useMemo(() => {
+    const setColorMode = (newValue: "dark" | "light") => {
+      const root = window.document.documentElement;
+      root.setAttribute("data-theme", newValue);
 
-			localStorage.setItem(COLOR_MODE_KEY, newValue);
-			Object.entries(COLORS).map(([name, colorByTheme]) => {
-				const cssVariable = `--color-${name}`;
+      localStorage.setItem(COLOR_MODE_KEY, newValue);
+      Object.entries(COLORS).map(([name, colorByTheme]) => {
+        const cssVariable = `--color-${name}`;
 
-				root.style.setProperty(cssVariable, colorByTheme[newValue]);
-			});
+        root.style.setProperty(cssVariable, colorByTheme[newValue]);
+      });
 
-			rawSetColorMode(newValue);
-		};
+      rawSetColorMode(newValue);
+    };
 
-		return { colorMode, setColorMode };
-	}, [colorMode, rawSetColorMode]);
+    return { colorMode, setColorMode };
+  }, [colorMode, rawSetColorMode]);
 
-	return (
-		<ThemeContext.Provider value={contextValue}>
-			{children}
-		</ThemeContext.Provider>
-	);
+  return (
+    <ThemeContext.Provider value={contextValue}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
