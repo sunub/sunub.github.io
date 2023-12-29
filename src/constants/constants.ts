2b33f0c4b2160a807be493b19190c1f483b5a4ab
sunub <bsc5672@gmail.com>
2023-12-29 23:43:43 +0900

    refactor : Color Prefer에 따른 색상을 css var을 키로 변경하는 색상을 값으로 갖게끔 코드 수정

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,46 +1,78 @@
-const COLORS = {
-  text: {
-    light: `oklch(21.08% 0.055 34.69)`,
-    dark: `oklch(100% 0 31.08)`,
-  },
-  background: {
-    light: `oklch(97.14% 0.011 31.07)`,
-    dark: `oklch(23.93% 0 0)`,
-  },
-  primary: {
-    light: `oklch(100% 0 0 / 0.8)`,
-    dark: `oklch(34% 0.019 229.64)`,
-  },
-  highlightColor: {
-    light: `oklch(70.8% 0.165 32.85)`,
-    dark: `oklch(64.86% 0.181 249.54)`,
-  },
-  header: {
-    light: `oklch(90.8% 0.046 29.64)`,
-    dark: `oklch(30.4% 0.005 247.98)`,
-  },
-  icon: {
-    light: `oklch(61.8% 0.027 30.58)`,
-    dark: `oklch(97.14% 0.011 31.07)`,
-  },
-  elevation: {
-    light: "oklch(100% 0 0)",
-    dark: "oklch(45.42% 0.007 239.95)",
-  },
-  bird: {
-    light: "oklch(73.44% 0.152 21.47)",
-    dark: "oklch(90.21% 0.055771670330652126 300.11937740281473)",
-  },
-  thumb: {
-    light: "oklch(92.54% 0.01 32.52)",
-    dark: "oklch(92.17% 0.039 291.94)",
-  },
-  thumbBackground: {
-    light: "oklch(53.74% 0.029 30.1)",
-    dark: "oklch(45.42% 0.007 239.95)",
-  },
+// const COLORS = {
+//   text: {
+//     light: `oklch(21.08% 0.055 34.69)`,
+//     dark: `oklch(100% 0 31.08)`,
+//   },
+//   background: {
+//     light: `oklch(97.14% 0.011 31.07)`,
+//     dark: `oklch(23.93% 0 0)`,
+//   },
+//   primary: {
+//     light: `oklch(100% 0 0 / 0.8)`,
+//     dark: `oklch(34% 0.019 229.64)`,
+//   },
+//   highlightColor: {
+//     light: `oklch(70.8% 0.165 32.85)`,
+//     dark: `oklch(64.86% 0.181 249.54)`,
+//   },
+//   header: {
+//     light: `oklch(90.8% 0.046 29.64)`,
+//     dark: `oklch(30.4% 0.005 247.98)`,
+//   },
+//   icon: {
+//     light: `oklch(61.8% 0.027 30.58)`,
+//     dark: `oklch(97.14% 0.011 31.07)`,
+//   },
+//   elevation: {
+//     light: "oklch(100% 0 0)",
+//     dark: "oklch(45.42% 0.007 239.95)",
+//   },
+//   bird: {
+//     light: "oklch(73.44% 0.152 21.47)",
+//     dark: "oklch(90.21% 0.055771670330652126 300.11937740281473)",
+//   },
+//   thumb: {
+//     light: "oklch(92.54% 0.01 32.52)",
+//     dark: "oklch(92.17% 0.039 291.94)",
+//   },
+//   thumbBackground: {
+//     light: "oklch(53.74% 0.029 30.1)",
+//     dark: "oklch(45.42% 0.007 239.95)",
+//   },
+// };
+
+// const COLOR_MODE_KEY = "theme-preference";
+
+// export { COLORS, COLOR_MODE_KEY };
+
+const LIGHT_COLORS: Record<string, string> = {
+  "--color-navlink": `oklch(42.14% 0.08 29.36)`,
+  "--color-text": `oklch(21.08% 0.055 34.69)`,
+  "--color-background": `oklch(97.14% 0.011 31.07)`,
+  "--color-primary": `oklch(100% 0 0 / 0.8)`,
+  "--color-highlight": `oklch(70.8% 0.165 32.85)`,
+  "--color-header": `oklch(90.8% 0.046 29.64)`,
+  "--color-icon": `oklch(61.8% 0.027 30.58)`,
+  "--color-elevation": `oklch(100% 0 0)`,
+  "--color-bird": `oklch(73.44% 0.152 21.47)`,
+  "--color-thumb": `oklch(92.54% 0.01 32.52)`,
+  "--color-thumb-background": `oklch(53.74% 0.029 30.1)`,
+};
+
+const DARK_COLORS: Record<string, string> = {
+  "--color-navlink": `oklch(90.21% 0.055771670330652126 300.11937740281473)`,
+  "--color-text": `oklch(100% 0 31.08)`,
+  "--color-background": `oklch(23.93% 0 0)`,
+  "--color-primary": `oklch(34% 0.019 229.64)`,
+  "--color-highlight": `oklch(64.86% 0.181 249.54)`,
+  "--color-header": `oklch(30.4% 0.005 247.98)`,
+  "--color-icon": `oklch(97.14% 0.011 31.07)`,
+  "--color-elevation": `oklch(45.42% 0.007 239.95)`,
+  "--color-bird": `oklch(90.21% 0.055771670330652126 300.11937740281473)`,
+  "--color-thumb": `oklch(92.17% 0.039 291.94)`,
+  "--color-thumb-background": `oklch(45.42% 0.007 239.95)`,
 };
 
 const COLOR_MODE_KEY = "theme-preference";
 
-export { COLORS, COLOR_MODE_KEY };
+export { LIGHT_COLORS, DARK_COLORS, COLOR_MODE_KEY };
