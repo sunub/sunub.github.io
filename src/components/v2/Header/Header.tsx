672ec874ab79264d9bf0fe31ef997ba2046dd028
sunub <bsc5672@gmail.com>
2024-01-16 00:22:11 +0900

    fix : Build Error

--- src/components/v2/Header/Header.tsx
+++ src/components/v2/Header/Header.tsx
@@ -1,21 +1,10 @@
 import * as Styled from "./Header.style";
-<<<<<<< HEAD
-=======
-import Link from "next/link";
-import Image from "next/image";
->>>>>>> refs/remotes/origin/sunub
 import Spacer from "@/components/Spacer";
 import ThemeToggler from "@/components/Theme/Toggler/ThemeToggler";
 import Logo from "./Logo";
 import Navigation from "./Navigation";
-<<<<<<< HEAD
 
 function Header() {
-=======
-import { Theme } from "type";
-
-function Header({ initColorTheme }: { initColorTheme: Theme | string }) {
->>>>>>> refs/remotes/origin/sunub
   return (
     <>
       <Spacer size={60} axis={"vertical"} />
@@ -26,14 +15,7 @@ function Header({ initColorTheme }: { initColorTheme: Theme | string }) {
             <Navigation />
           </Styled.LogoAndNavWrapper>
           <Styled.ThemeWrapper>
-<<<<<<< HEAD
             <ThemeToggler maskId="desktop-header-theme-toggler" />
-=======
-            <ThemeToggler
-              initColorTheme={initColorTheme}
-              maskId="desktop-header-theme-toggler"
-            />
->>>>>>> refs/remotes/origin/sunub
           </Styled.ThemeWrapper>
         </Styled.Header>
       </Styled.RootWrapper>
