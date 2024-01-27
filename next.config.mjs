82f137f153b31a08ec4dcd8f34f180b752caba6c
sunub <bsc5672@gmail.com>
2024-01-27 23:22:28 +0900

    feat(app): Redirect path 추가 및 Header 규칙 선언
    블로그 내의 페이지간 이동 속도를 증가 시키기 위한 목적으로 앱에 redirect할 path를 데이터 베이스를 이용하여 등록하고 추가적인 Header 규칙을 선언하였다.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+import { sql } from "@vercel/postgres";
 
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
@@ -10,6 +11,64 @@ const nextConfig = {
     styledComponents: true,
   },
   output: "standalone",
+  redirects: async () => {
+    if (process.env.POSTGRES_URL === undefined) {
+      return [];
+    }
+
+    const tableData = await sql`
+      SELECT source, destination, permanent 
+      FROM redirects;
+    `;
+
+    const redirects = tableData.rows;
+
+    return redirects.map(({ source, destination, permanent }) => ({
+      source,
+      destination,
+      permanent: !!permanent,
+    }));
+  },
+  headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
+const ContentSecurityPolicy = `
+    default-src 'self' vercel.live;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live va.vercel-scripts.com;
+    style-src 'self' 'unsafe-inline';
+    img-src * blob: data:;
+    media-src 'none';
+    connect-src *;
+    font-src 'self' data:;
+`;
+
+const securityHeaders = [
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload",
+  },
+];
 export default nextConfig;
