d9c7aabf02fda47f260f6720c109aa7c96697792
sunub <bsc5672@gmail.com>
2024-01-29 17:50:29 +0900

    perf : 블로그 포스트 로드하는 방식의 변경

--- src/db/firebase.mjs
+++ src/db/firebase.mjs
@@ -2,17 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCLsNVaJMGq_PXdn7ORGqMPvX0ls8bpf8A",
-  authDomain: "sunub-blog.firebaseapp.com",
-  databaseURL:
-    "https://sunub-blog-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "sunub-blog",
-  storageBucket: "sunub-blog.appspot.com",
-  messagingSenderId: "500147997868",
-  appId: "1:500147997868:web:f13bd764a034de62d92309",
-  measurementId: "G-CPS9PG4E6C",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
