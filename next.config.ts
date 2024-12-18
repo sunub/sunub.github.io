246c00bb2e592d044c950e0d082f8bca42f71797
sunub <bsc5672@gmail.com>
2024-12-18 15:23:07 +0900

    feat : 영상이 같은 서버 내에서 사용되는 리소스일 경우 자원 보안 정책에 걸리지 않게 끔 설정 수정

--- next.config.ts
+++ next.config.ts
@@ -67,7 +67,7 @@ const ContentSecurityPolicy = `
     script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live va.vercel-scripts.com;
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
     img-src * blob: data:;
-    media-src 'none';
+    media-src 'self';
     connect-src *;
 `;
 
