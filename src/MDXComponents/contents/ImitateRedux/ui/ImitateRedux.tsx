fb41e7099a7f7de3997055abfbcb0752a14f8a52
sunub <bsc5672@gmail.com>
2025-03-08 22:22:21 +0900

    feat : 기존의 블로그 포스트 내에서 사용될 컴포넌트를 좀 더 명확한 폴더 구조로 변경

---
+++ src/MDXComponents/contents/ImitateRedux/ui/ImitateRedux.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { memo, useCallback, useEffect } from "react";
+import { useStoreSelector } from "../hook/useStoreSelector";
+import { createStore } from "../model/store";
+
+const selectStore = createStore({
+  count: 0,
+  text: "Hello",
+});
+
+function SelectorCounter() {
+  const count = useStoreSelector(
+    selectStore,
+    useCallback((state: ReturnType<typeof selectStore.get>) => state.count, []),
+  );
+
+  function handleClick() {
+    selectStore.set((prev) => ({ ...prev, count: prev.count + 1 }));
+  }
+
+  return (
+    <div>
+      <h3>Counter: {count}</h3>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleClick}
+      >
+        Increment
+      </button>
+    </div>
+  );
+}
+
+const TextEditor = memo(() => {
+  const text = useStoreSelector(
+    selectStore,
+    useCallback((state: ReturnType<typeof selectStore.get>) => state.text, []),
+  );
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    selectStore.set((prev) => ({ ...prev, text: e.target.value }));
+  }
+
+  return (
+    <div>
+      <h3>Text Editor</h3>
+      <input value={text} onChange={handleChange} />
+    </div>
+  );
+});
+
+function ImitateRedux() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 py-4 border-2 border-gray-500 rounded-md mb-4">
+      <div className="flex  items-center justify-center  gap-4">
+        <div className="flex items-center justify-center flex-col">
+          <SelectorCounter />
+        </div>
+        <div className="flex items-center justify-center flex-col">
+          <TextEditor />
+        </div>
+      </div>
+      <p>위의 버튼을 클릭하면서 랜더링이 어떻게 일어나는지 확인 해주세요!</p>
+    </div>
+  );
+}
+
+export { ImitateRedux };
