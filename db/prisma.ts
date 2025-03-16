75ed930e8062f3e06937bdaf3cefa51dd74f2a85
sunub <bsc5672@gmail.com>
2025-03-16 16:43:40 +0900

    feat : 불필요한 이벤트 로그 삭제

--- db/prisma.ts
+++ db/prisma.ts
@@ -8,9 +8,8 @@ const logThreshold = 0;
 const prisma = singleton("prisma", () => {
   const client = new PrismaClient({
     log: [
-      { level: "query", emit: "event" },
-      { level: "query", emit: "event" },
-      { level: "query", emit: "event" },
+      { level: "error", emit: "event" },
+      { level: "warn", emit: "event" },
       { level: "query", emit: "event" },
     ],
     datasources: {
@@ -25,12 +24,12 @@ const prisma = singleton("prisma", () => {
       e.duration < logThreshold * 1.1
         ? "green"
         : e.duration < logThreshold * 1.2
-          ? "blue"
-          : e.duration < logThreshold * 1.3
-            ? "yellow"
-            : e.duration < logThreshold * 1.4
-              ? "redBright"
-              : "red";
+        ? "blue"
+        : e.duration < logThreshold * 1.3
+        ? "yellow"
+        : e.duration < logThreshold * 1.4
+        ? "redBright"
+        : "red";
     const dur = chalk[color](`${e.duration}ms`);
     console.info(`prisma:query - ${dur} - ${e.query}`);
   });
