fb644477e6fe135d611f52f1c35ab77993cd47ad
sunub <bsc5672@gmail.com>
2025-03-26 01:12:32 +0900

    feat : IO 작업을 Worker를 사용하여 분산
    - 기존에는 fs 모듈을 활용해 mdx 파일들을 읽어왔지만 worker를 사용하여 분산 시킴
    - 각각의 폴더는 독립적이로 영향을 주지 않기 때문에 worker를 사용하는 것이 성능 향상에 도움이 될 것이라 판단
    - lruCache의 경우 서버리스 환경에서 인스턴스가 계속해서 새롭게 생성되는 것으로 인해 성능 향상을 느낄 수 없었음
    - 그래서 선택한 것이 IO작업을 분리하고 기존의 시간 순 정렬을 이진 탐색을 활용해 시간을 단축 시키는 것이 유의미할 것이라 판단함

---
+++ db/blog/blog.ts
@@ -0,0 +1,283 @@
+"use server";
+
+import { z } from "zod";
+import chalk from "chalk";
+import path from "node:path";
+import chokidar from "chokidar";
+import { Worker } from "node:worker_threads";
+import { type WorkerMessage } from "./cache";
+import { ROOT_BLOG_PATH } from "./constants";
+import {
+  FrontMatter,
+  FrontMatterSchema,
+  PostCategory,
+  PostCategorySchema,
+} from "@/types/schema";
+import { LRUCacheInstance } from "./cache";
+import { LRUCache } from "lru-cache";
+import { debounce } from "@/shared/utils/debounce";
+
+type WorkerList = {
+  path: string;
+  tag: PostCategory;
+};
+
+type PostData = {
+  frontmatter: z.infer<typeof FrontMatterSchema>;
+  content: string[];
+};
+
+type PostKey = `posts:${PostCategory}:${string}`;
+type PostContentKey = `posts:content:${PostCategory}:${string}`;
+
+const workerIdList: WorkerList[] = [
+  {
+    path: path.resolve(process.cwd(), "./db/workers/algorithm.worker.js"),
+    tag: "algorithm",
+  },
+  {
+    path: path.resolve(process.cwd(), "./db/workers/code.worker.js"),
+    tag: "code",
+  },
+  {
+    path: path.resolve(process.cwd(), "./db/workers/cs.worker.js"),
+    tag: "cs",
+  },
+  {
+    path: path.resolve(process.cwd(), "./db/workers/web.worker.js"),
+    tag: "web",
+  },
+];
+
+class Blog {
+  #__workers: Map<PostCategory, Worker> = new Map();
+  #__updateLock: Promise<void> = Promise.resolve();
+  #__lruCache: LRUCache<string, any, unknown> = LRUCacheInstance;
+  sortedPosts: PostData[] = [];
+  blogContents: Map<PostContentKey, string[]> = new Map();
+  algorithm: Map<string, FrontMatter> = new Map();
+  code: Map<string, FrontMatter> = new Map();
+  cs: Map<string, FrontMatter> = new Map();
+  web: Map<string, FrontMatter> = new Map();
+
+  isInitialized = false;
+
+  private static instance: Blog | null = null;
+  private static initializationPromise: Promise<Blog> | null = null;
+
+  constructor() {
+    this.#__lruCache = LRUCacheInstance;
+    const watcher = chokidar.watch(ROOT_BLOG_PATH, {
+      persistent: true,
+      ignoreInitial: true,
+    });
+    watcher.on("change", (changeFilePath) =>
+      debounce(() => this.handleFileChange(changeFilePath), 100)
+    );
+  }
+
+  private insertPostSorted(post: {
+    cacheKey: string;
+    frontmatter: z.infer<typeof FrontMatterSchema>;
+    content: string[];
+  }) {
+    const postDate = new Date(post.frontmatter.date).getTime();
+    let low = 0,
+      high = this.sortedPosts.length;
+    while (low < high) {
+      const mid = Math.floor((low + high) / 2);
+      const midDate = new Date(
+        this.sortedPosts[mid].frontmatter.date
+      ).getTime();
+
+      if (postDate > midDate) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+    this.sortedPosts.splice(low, 0, post);
+  }
+
+  getPostContent(category: PostCategory, slug: string): string[] {
+    const postKey = this.#__createPostContentKey(category, slug);
+    return this.blogContents.get(postKey) || [];
+  }
+
+  async handleFileChange(changeFilePath: string) {
+    this.#__withLock(async () => {
+      const splitedPath = changeFilePath.split("/");
+      let [category, slug] = splitedPath.slice(-2);
+      slug = slug.split(".")[0];
+      const parsedCategory = PostCategorySchema.safeParse(category);
+      if (!parsedCategory.success) {
+        console.error(
+          chalk.redBright(
+            `카테고리가 잘못되었습니다. ${category}는 올바른 카테고리가 아닙니다.`
+          )
+        );
+        return;
+      }
+      const worker = this.#__workers.get(parsedCategory.data);
+      worker?.postMessage({
+        type: "change",
+        content: changeFilePath,
+        slug,
+      });
+    });
+  }
+
+  async #__withLock<T>(fn: () => Promise<T>): Promise<T> {
+    let previousLock: Promise<void> = this.#__updateLock;
+    let release: () => void;
+    this.#__updateLock = new Promise<void>((resolve) => {
+      release = resolve;
+    });
+    await previousLock;
+    try {
+      return await fn();
+    } finally {
+      release!();
+    }
+  }
+
+  #__createCacheKey(category: PostCategory, slug: string): string {
+    return `cache:${category}:${slug}`;
+  }
+
+  #__createPostKey(category: PostCategory, slug: string): PostKey {
+    return `posts:${category}:${slug}`;
+  }
+
+  #__createPostContentKey(
+    category: PostCategory,
+    slug: string
+  ): PostContentKey {
+    return `posts:content:${category}:${slug}`;
+  }
+
+  static async getInstance(): Promise<Blog> {
+    if (Blog.instance && Blog.instance.isInitialized) {
+      return Blog.instance;
+    }
+
+    Blog.initializationPromise = (async () => {
+      try {
+        if (!Blog.instance) {
+          Blog.instance = new Blog();
+        }
+
+        if (!Blog.instance.isInitialized) {
+          await Blog.instance.initialize();
+          Blog.instance.isInitialized = true;
+        }
+
+        return Blog.instance;
+      } catch (error) {
+        Blog.initializationPromise = null;
+        console.error("Blog 인스턴스 초기화 중 오류 발생:", error);
+        throw error;
+      }
+    })();
+
+    return Blog.initializationPromise;
+  }
+
+  async #__storeResultByCategoryAndSlug(
+    category: PostCategory,
+    slug: string,
+    frontmatter: z.infer<typeof FrontMatterSchema>,
+    content: string[]
+  ) {
+    const cacheKey = this.#__createCacheKey(category, slug);
+    const postKey = this.#__createPostKey(category, slug);
+    const PostContentKey = this.#__createPostContentKey(category, slug);
+
+    this[category].set(postKey, { ...frontmatter });
+    this.#__lruCache.set(cacheKey, { data: frontmatter, content });
+    this.blogContents.set(PostContentKey, content);
+
+    const post = { frontmatter, content, cacheKey };
+    this.insertPostSorted(post);
+  }
+
+  getPostMetadataBySlug(category: PostCategory, slug: string) {
+    const postKey = this.#__createPostKey(category, slug);
+    return this[category].get(postKey);
+  }
+
+  async initialize(): Promise<void> {
+    const workerPromises = workerIdList.map(({ path, tag }) => {
+      return new Promise<void>((resolve, reject) => {
+        let frontmatter: z.infer<typeof FrontMatterSchema> | null = null;
+        let content: string[] = [];
+        const worker = new Worker(path, {
+          workerData: { tag },
+        });
+        this.#__workers.set(tag, worker);
+
+        worker.postMessage(tag);
+        worker.on("message", (message: WorkerMessage) => {
+          switch (message.type) {
+            case "data":
+              if (message.frontmatter) {
+                frontmatter = message.frontmatter;
+              }
+              if (message.content) {
+                content.push(message.content);
+              }
+              break;
+            case "fileComplete":
+              if (frontmatter && content.length) {
+                this.#__storeResultByCategoryAndSlug(
+                  tag,
+                  message.slug,
+                  frontmatter,
+                  content
+                );
+              }
+              frontmatter = null;
+              content = [];
+              break;
+            case "done":
+              console.log(
+                chalk.greenBright(
+                  `${message.content} 작업이 모두 완료 되었습니다.`
+                )
+              );
+              resolve();
+              break;
+            case "error":
+              console.error(message.content);
+              break;
+          }
+        });
+
+        worker.on("error", (error) => {
+          console.error(`Worker error for tag ${tag}:`, error);
+          reject(error);
+        });
+
+        worker.on("exit", (code) => {
+          if (code !== 0) {
+            const errMsg = `Worker for tag ${tag} stopped with exit code ${code}`;
+            console.error(errMsg);
+            reject(new Error(errMsg));
+          }
+        });
+      });
+    });
+    await Promise.all(workerPromises);
+  }
+
+  async getPostsMetadataByCategory(category: PostCategory) {
+    const targetPosts = this[category];
+    return [...targetPosts.values()];
+  }
+}
+
+const getBlogInstance = async () => {
+  return Blog.getInstance();
+};
+
+export default getBlogInstance;
