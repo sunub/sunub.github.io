9dc367d80fb0d28bb3d4d6aad8ed7a40a89387e2
sunub <bsc5672@gmail.com>
2025-03-26 14:33:08 +0900

    feat : 로딩 애니메이션 수정

---
+++ src/app/loading.style.ts
@@ -0,0 +1,49 @@
+"use client";
+
+import styled from "styled-components";
+
+export const ArticleHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 100cqw;
+
+  text-align: center;
+  word-break: break-all;
+
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+`;
+
+export const PostTitle = styled.h1`
+  font-weight: 900;
+  font-size: 3rem;
+  line-height: 3.5rem;
+  margin-bottom: 2rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  text-wrap: pretty;
+`;
+
+export const ArticleWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  gap: 2.25rem;
+`;
+
+export const Article = styled.article`
+  width: 100%;
+  height: 100%;
+
+  max-width: 850px;
+  font-size: 18px;
+  line-height: calc(1.7rem + 0.8rem);
+`;
