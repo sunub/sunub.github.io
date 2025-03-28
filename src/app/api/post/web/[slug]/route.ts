49009e0b0a10c7e1e37c47746fa46cbf73248b96
sunub <bsc5672@gmail.com>
2025-03-29 00:38:33 +0900

    feat : 블로그 포스트의 내용을 API Fetch를 이용해 읽어오게끔 방식 수정

---
+++ src/app/api/post/web/[slug]/route.ts
@@ -0,0 +1,24 @@
+import matter from "gray-matter";
+import { createReadStream } from "node:fs";
+import { getPostPath } from "@/shared/utils/getPostPath";
+
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ slug: string }> }
+) {
+  const { slug } = await params;
+  const filePath = getPostPath("web", slug);
+  const readStream = createReadStream(filePath, { encoding: "utf-8" });
+
+  const chunks: string[] = [];
+  for await (const chunk of readStream) {
+    chunks.push(chunk);
+  }
+
+  const fileContent = chunks.join("");
+  const { content } = matter(fileContent);
+  return new Response(JSON.stringify({ content }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
