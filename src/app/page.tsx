24b15c6a311aa7f0ad3a5c8280d55a6ba377d7af
sunub <bsc5672@gmail.com>
2025-03-28 16:28:43 +0900

    feat : 성능 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,20 +10,20 @@ import { NewestPostSkeleton } from "@/components/Skeletons";
 
 const categories = ["cs", "web", "code", "algorithm"];
 
-const NewestPost = dynamic(() => import("@/components/Main/NewestPost"), {
-  loading: () => <NewestPostSkeleton />,
-  ssr: true,
-});
+// const NewestPost = dynamic(() => import("@/components/Main/NewestPost"), {
+//   loading: () => <NewestPostSkeleton />,
+//   ssr: true,
+// });
 
 async function Page() {
-  const recentPostsMetadata = await getRecentPostsMetadata(10);
+  // const recentPostsMetadata = await getRecentPostsMetadata(10);
   return (
     <React.Fragment>
       <Styled.HeaderContentsWrapper>
         <HeroImage />
         <Wave />
       </Styled.HeaderContentsWrapper>
-      <div id="blog-main__recently-post-list">
+      {/* <div id="blog-main__recently-post-list">
         <Styled.MainWrapper>
           <Suspense fallback={<NewestPostSkeleton />}>
             <NewestPost initialPosts={recentPostsMetadata} />
@@ -32,7 +32,7 @@ async function Page() {
             <Categories categories={categories} />
           </Styled.RightSideWrapper>
         </Styled.MainWrapper>
-      </div>
+      </div> */}
     </React.Fragment>
   );
 }
