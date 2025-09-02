1b3e94a5bb48f35865ec76b0cbdbf0fb64cebe70
sunub <bsc5672@gmail.com>
2025-09-02 22:18:42 +0900

    chore : Eslint, Prettier 규칙 추가 및 에디터 설정 추가
    - eslint 설정의 경우 모노레포 환경에 맞춰 필요한 rule을 구분하여 작성하였습니다.
    - prettier 설정의 경우 toss에서 사용하는 규칙을 참고하였습니다.
    - eslint 설정 중 유틸함수를 생성하거나 next.js를 사용할 때 불필요하게 에러를 발생시키는 오류를 수정하였습니다.

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -10,7 +10,25 @@ module.exports = {
   semi: true,
   singleQuote: true,
   tabWidth: 2,
-  importOrder: ['<THIRD_PARTY_MODULES>', '^@(.*)$', '^[.]/', '^[.]{2,}/'],
+  trailingComma: 'es5',
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+  importOrder: ['^vitest', '<THIRD_PARTY_MODULES>', '^@(.*)$', '^[.]/', '^[.]{2,}/'],
   importOrderSortSpecifiers: true,
   importOrderCaseInsensitive: true,
+  overrides: [
+    {
+      files: [
+        'frontend/src/**/index.ts',
+        'backend/src/**/index.ts',
+        'fx_utils/src/**/index.ts',
+        'frontend/db/**/index.ts',
+      ],
+      options: {
+        plugins: [
+          require.resolve('@trivago/prettier-plugin-sort-imports'),
+          require.resolve('prettier-plugin-sort-re-exports'),
+        ],
+      },
+    },
+  ],
 };
