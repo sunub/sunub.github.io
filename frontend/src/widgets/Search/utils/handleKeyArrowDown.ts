38e85c5d618386ad0d65f2cbb854fb76dce53a01
sunub <bsc5672@gmail.com>
2025-09-02 23:53:30 +0900

    feat : 키보드를 이용하여 검색 결과에 접근할 수 있게끔 키보드 핸들러 로직을 추가
    - 위 아래로 이동할 때 필요한 로직을 별도의 유틸함수로 관리하여 검색 결과의 내부를 키보드의 방향키 만으로 접근할 수 있게끔
    하였습니다.

---
+++ frontend/src/widgets/Search/utils/handleKeyArrowDown.ts
@@ -0,0 +1,36 @@
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import { RefObject } from 'react';
+import { scrollToElement } from './scrollToElement';
+
+export function handleKeyArrowDown(e: KeyboardEvent, indexRef: RefObject<number>, router: AppRouterInstance) {
+  const key = e.key;
+  if (key !== 'ArrowDown') {
+    return;
+  }
+
+  e.preventDefault();
+
+  const searchResultList = document.getElementById('search-results-listbox');
+  const listItems = searchResultList?.querySelectorAll('li');
+  if (!listItems || listItems.length === 0) {
+    return;
+  }
+
+  if (indexRef.current >= 0) {
+    listItems[indexRef.current].setAttribute('aria-selected', 'false');
+  }
+
+  indexRef.current = (indexRef.current + 1) % listItems.length;
+  const currentItem = listItems[indexRef.current];
+  const link = currentItem.querySelector('a');
+  const href = link?.getAttribute('href');
+  if (link && href) {
+    router.prefetch(href);
+  }
+
+  if (link && searchResultList) {
+    currentItem.setAttribute('aria-selected', 'true');
+    scrollToElement(currentItem as HTMLElement, searchResultList);
+    (link as HTMLElement).focus();
+  }
+}
