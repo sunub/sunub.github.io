a2ede7d4a80c934b87f97881990814a02df7a346
sunub <bsc5672@gmail.com>
2025-09-02 23:01:42 +0900

    feat : 타입 안정성을 위해 검색 결과에 대한 검증을 위한 Schema 작성

--- frontend/src/app/api/search/route.ts
+++ frontend/src/app/api/search/route.ts
@@ -1,33 +1,41 @@
-import getBlogInstance from "db/blog/blog";
-import { findMatches } from "./utils/findMatches";
+import { getAllPostsFrontmatter } from 'db/blog/api';
+import { z } from 'zod/v4';
+import { PostFrontMatterSchema } from '@/db/blog/Schema';
+import { findMatches } from './utils/findMatches';
+
+const SearchResultSchema = z.object({
+  post: PostFrontMatterSchema,
+  titleMatches: z.array(z.string()),
+  summaryMatches: z.array(z.string()),
+});
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const query = searchParams.get("query");
+  const query = searchParams.get('query');
   if (!query) {
     return new Response(JSON.stringify({ results: [] }), {
       status: 200,
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
     });
   }
-  console.log("search query", query);
 
-  const blog = await getBlogInstance();
   const results = [];
-  for (const post of blog.sortedPosts) {
+  const posts = await getAllPostsFrontmatter();
+  for (const post of posts) {
     const { title, summary } = post.frontmatter;
     const titleHits = findMatches(query, title);
-    // const summaryHits = findMatches(query, summary);
+    const summaryHits = findMatches(query, summary);
     if (titleHits.length) {
       results.push({
         post,
         titleMatches: titleHits,
-        // summaryMatches: summaryHits,
+        summaryMatches: summaryHits,
       });
     }
   }
+  z.array(SearchResultSchema).parse(results);
   return new Response(JSON.stringify({ results }), {
     status: 200,
-    headers: { "Content-Type": "application/json" },
+    headers: { 'Content-Type': 'application/json' },
   });
 }
