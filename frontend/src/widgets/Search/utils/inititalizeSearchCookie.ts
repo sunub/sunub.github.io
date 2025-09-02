9f0892d2d0ac582f7eb734e7954baeccb5e6e5bc
sunub <bsc5672@gmail.com>
2025-09-02 23:53:37 +0900

    refactor : Eslint 규칙에 맞게끔 코드 수정 및 Prettier 포맷을 적용

--- frontend/src/widgets/Search/utils/inititalizeSearchCookie.ts
+++ frontend/src/widgets/Search/utils/inititalizeSearchCookie.ts
@@ -1,12 +1,12 @@
-"use server";
+'use server';
 
-import { cookies } from "next/headers";
+import { cookies } from 'next/headers';
 
 export async function initializeSearchCookie() {
   const cookieStore = await cookies();
   const cookieValue = JSON.stringify({ isSearchOpen: false });
-  cookieStore.set("search", cookieValue, {
-    sameSite: "strict",
-    path: "/",
+  cookieStore.set('search', cookieValue, {
+    sameSite: 'strict',
+    path: '/',
   });
 }
