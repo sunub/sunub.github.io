14976e00273fd4c7c478fc53ab20df996d16f1df
sunub <bsc5672@gmail.com>
2025-09-23 16:50:57 +0900

    refactor : GSAP를 React에서 사용하는 방식에 맞게끔 수정
    - 기존에는 특정 DOM에 의존하여 애니메이션을 구현하였지만 이를 ref를 사용하여 구현되도록 수정

--- frontend/src/components/Main/BlogPost/hooks/useLoadPostAnimation.ts
+++ frontend/src/components/Main/BlogPost/hooks/useLoadPostAnimation.ts
@@ -1,15 +1,18 @@
 import gsap from 'gsap';
 import { useEffect } from 'react';
 
-export function useLoadPostAnimation() {
+export function useLoadPostAnimation(containerRef: React.RefObject<HTMLElement | null>) {
   useEffect(() => {
-    const elements = gsap.utils.toArray('#blog-post__recently-post-list > *');
+    if (!containerRef.current?.children) return;
+
+    const elements = Array.from(containerRef.current.children);
+
     gsap.set(elements, {
       y: -10,
       opacity: 0,
     });
 
-    gsap.to(elements, {
+    const animation = gsap.to(elements, {
       duration: 0.8,
       y: 0,
       opacity: 1,
@@ -18,5 +21,9 @@ export function useLoadPostAnimation() {
       ease: 'power2.out',
       force3D: true,
     });
-  }, []);
+    
+    return () => {
+      animation.kill();
+    }
+  }, [containerRef]);
 }
