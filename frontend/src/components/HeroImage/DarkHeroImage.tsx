38c1f42c49242b4d2a141900df6550454d9c8e1a
sunub <bsc5672@gmail.com>
2025-11-21 14:54:13 +0900

    refactor : Formatter를 사용하여 리펙토링

--- frontend/src/components/HeroImage/DarkHeroImage.tsx
+++ frontend/src/components/HeroImage/DarkHeroImage.tsx
@@ -1,26 +1,40 @@
 import { memo } from "react";
-import { DrakHeroImageWapper, Clouds, Bridge, Cars, DarkShadow } from "./HeroImage.style";
+import {
+	Bridge,
+	Cars,
+	Clouds,
+	DarkShadow,
+	DrakHeroImageWapper,
+} from "./HeroImage.style";
 
 interface DarkHeroImageProps {
-  $isVisible: boolean;
+	$isVisible: boolean;
 }
 
 const DarkHeroImage = memo(({ $isVisible }: DarkHeroImageProps) => {
-  const cloudStyle = { '--clouds-opacity': 'var(--color-dark-heroimage)' } as React.CSSProperties;
-  const bridgeStyle = { '--bridge-opacity': 'var(--color-dark-heroimage)' } as React.CSSProperties;
-  const carsStyle = { '--cars-opacity': 'var(--color-dark-heroimage)' } as React.CSSProperties;
-  const shadowStyle = { opacity: 'var(--color-dark-heroimage)' } as React.CSSProperties;
+	const cloudStyle = {
+		"--clouds-opacity": "var(--color-dark-heroimage)",
+	} as React.CSSProperties;
+	const bridgeStyle = {
+		"--bridge-opacity": "var(--color-dark-heroimage)",
+	} as React.CSSProperties;
+	const carsStyle = {
+		"--cars-opacity": "var(--color-dark-heroimage)",
+	} as React.CSSProperties;
+	const shadowStyle = {
+		opacity: "var(--color-dark-heroimage)",
+	} as React.CSSProperties;
 
-  return (
-    <DrakHeroImageWapper $isVisible={$isVisible}>
-      <Clouds $bgUrl="/assets/dark_clouds.avif" style={cloudStyle} />
-      <Bridge $bgUrl="/assets/dark_bridge.avif" style={bridgeStyle} />
-      <Cars $bgUrl="/assets/dark_cars.avif" style={carsStyle} />
-      <DarkShadow style={shadowStyle} />
-    </DrakHeroImageWapper>
-  );
+	return (
+		<DrakHeroImageWapper $isVisible={$isVisible}>
+			<Clouds $bgUrl="/assets/dark_clouds.avif" style={cloudStyle} />
+			<Bridge $bgUrl="/assets/dark_bridge.avif" style={bridgeStyle} />
+			<Cars $bgUrl="/assets/dark_cars.avif" style={carsStyle} />
+			<DarkShadow style={shadowStyle} />
+		</DrakHeroImageWapper>
+	);
 });
 
-DarkHeroImage.displayName = 'DarkHeroImage';
+DarkHeroImage.displayName = "DarkHeroImage";
 
 export default DarkHeroImage;
--- frontend/src/components/HeroImage/LightHeroImage.tsx
+++ frontend/src/components/HeroImage/LightHeroImage.tsx
@@ -1,24 +1,38 @@
 import { memo } from "react";
-import { LightHeroImageWapper, Clouds, Bridge, Cars, LightShadow } from "./HeroImage.style";
+import {
+	Bridge,
+	Cars,
+	Clouds,
+	LightHeroImageWapper,
+	LightShadow,
+} from "./HeroImage.style";
 
 interface LightHeroImageProps {
-  $isVisible: boolean;
+	$isVisible: boolean;
 }
 
 const LightHeroImage = memo(({ $isVisible }: LightHeroImageProps) => {
-  const cloudStyle = { '--clouds-opacity': 'var(--color-light-heroimage)' } as React.CSSProperties;
-  const bridgeStyle = { '--bridge-opacity': 'var(--color-light-heroimage)' } as React.CSSProperties;
-  const carsStyle = { '--cars-opacity': 'var(--color-light-heroimage)' } as React.CSSProperties;
-  const shadowStyle = { opacity: 'var(--color-light-heroimage)' } as React.CSSProperties;
+	const cloudStyle = {
+		"--clouds-opacity": "var(--color-light-heroimage)",
+	} as React.CSSProperties;
+	const bridgeStyle = {
+		"--bridge-opacity": "var(--color-light-heroimage)",
+	} as React.CSSProperties;
+	const carsStyle = {
+		"--cars-opacity": "var(--color-light-heroimage)",
+	} as React.CSSProperties;
+	const shadowStyle = {
+		opacity: "var(--color-light-heroimage)",
+	} as React.CSSProperties;
 
-  return (
-    <LightHeroImageWapper $isVisible={$isVisible}>
-      <Clouds $bgUrl="/assets/clouds.avif" style={cloudStyle} />
-      <Bridge $bgUrl="/assets/bridge.avif" style={bridgeStyle} />
-      <Cars $bgUrl="/assets/cars.avif" style={carsStyle} />
-      <LightShadow style={shadowStyle} />
-    </LightHeroImageWapper>
-  );
+	return (
+		<LightHeroImageWapper $isVisible={$isVisible}>
+			<Clouds $bgUrl="/assets/clouds.avif" style={cloudStyle} />
+			<Bridge $bgUrl="/assets/bridge.avif" style={bridgeStyle} />
+			<Cars $bgUrl="/assets/cars.avif" style={carsStyle} />
+			<LightShadow style={shadowStyle} />
+		</LightHeroImageWapper>
+	);
 });
 
 LightHeroImage.displayName = "LightHeroImage";
