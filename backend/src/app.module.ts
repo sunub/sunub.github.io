df378ec42b5af416747e12d501b93426fbaed460
sunub <bsc5672@gmail.com>
2025-11-26 15:41:24 +0900

    feat : 테스트 및 Vercel 환경에서 접근이 가능하도록 추가적인 앱 설정 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AppController } from "./app.controller";
 import { PostsModule } from "./posts/posts.module";
 import { SearchModule } from "./search/search.module";
 
 @Module({
-	imports: [PostsModule, SearchModule],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		PostsModule,
+		SearchModule,
+	],
+	controllers: [AppController],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,18 @@
 import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	const configService = app.get(ConfigService);
+	const originString = configService.get<string>("CORS_ORIGIN") || "";
+	const allowedOrigins = originString.split(",").map((url) => url.trim());
+
+	app.enableCors({
+		origin: allowedOrigins,
+		credentials: true,
+	});
 	app.useGlobalPipes(new ValidationPipe());
 	await app.listen(process.env.PORT ?? 3000);
 }
