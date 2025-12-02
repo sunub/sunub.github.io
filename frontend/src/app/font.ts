89c73de232aca2f14b32437795c34ee2ffe65395
sunub <bsc5672@gmail.com>
2025-12-03 00:47:26 +0900

    feat : 폰트를 subset을 사용하여 최적화 수행

--- frontend/src/app/font.ts
+++ frontend/src/app/font.ts
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 
 export const pretendardRegular = localFont({
-	src: "../../public/fonts/Pretendard-Regular.woff2",
+	src: "../../public/fonts/Pretendard-Regular.subset.woff2",
 	display: "swap",
 	style: "normal",
 	variable: "--pretendard-font-regular",
@@ -11,6 +11,7 @@ export const pretendardRegular = localFont({
 export const craftyGirls = localFont({
 	src: "../../public/fonts/CraftyGirls-Regular.woff2",
 	display: "swap",
+	weight: "400",
 	style: "normal",
 	variable: "--crafty-girls-font",
 	fallback: ["system-ui", "sans-serif"],
