3e659d3804c1586a645d4d4ac8eeac3e52fa2f0b
sunub <bsc5672@gmail.com>
2025-12-02 23:44:34 +0900

    feat: HeroImagePreload 컴포넌트 추가 및 이미지 프리로드 로직 제거

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -11,6 +11,7 @@ import { DARK_COLORS, LIGHT_COLORS } from "@/constants/constants";
 import { AnimatePresenceWrapper } from "@/features/AnimatePresenceWrapper";
 import { craftyGirls, pretendardRegular } from "./font";
 import { GlobalStyle, ShikiOverrieds } from "./GlobalStyle"; // 새로 만든 파일 import
+import { HeroImagePreload } from "@/components/HeroImage/HeroImagePreload";
 
 export const metadata: Metadata = {
 	metadataBase: new URL("https://sunub.vercel.app"),
@@ -88,6 +89,7 @@ export default async function RootLayout({
 					name="description"
 					content="sunub가 만든 개인 블로그입니다. 주로 프론트엔드 개발과 관련된 여러 지식들을 다루지만 이외에도 다양한 개발 지식을 공유하기 위한 사이트입니다."
 				/>
+				<HeroImagePreload theme={theme} />
 				<Script id="theme-script" strategy="beforeInteractive">
 					{initSetColorsByThemeFn}
 				</Script>
--- frontend/src/components/HeroImage/HeroImage.tsx
+++ frontend/src/components/HeroImage/HeroImage.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useContext } from "react";
-import { preload } from "react-dom";
 import type { Theme } from "type";
 import { ThemeContext } from "../Theme/ThemeProvider";
 import DarkHeroImage from "./DarkHeroImage";
@@ -16,16 +15,6 @@ function HeroImage({ initialTheme }: HeroImageProps) {
 	const { colorTheme } = useContext(ThemeContext);
 	const currentTheme = colorTheme || initialTheme || "light";
 	const isDarkTheme = currentTheme === "dark";
-
-	if (isDarkTheme) {
-		preload("/assets/dark_bridge.avif", { as: "image" });
-		preload("/assets/dark_clouds.avif", { as: "image" });
-		preload("/assets/dark_cars.avif", { as: "image" });
-	} else {
-		preload("/assets/bridge.avif", { as: "image" });
-		preload("/assets/clouds.avif", { as: "image" });
-		preload("/assets/cars.avif", { as: "image" });
-	}
 
 	return (
 		<RootWrapper suppressHydrationWarning={true}>
