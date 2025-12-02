18258b0b72b10d6d498b2585e35e5d8f3b86295c
sunub <bsc5672@gmail.com>
2025-12-03 00:28:36 +0900

    feat : HeroImage 동적 import 방식으로 변경

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,6 @@ import dynamic from "next/dynamic";
 import { cookies } from "next/headers";
 import type { Theme } from "type";
 import { Wave } from "@/components/Header/Wave";
-import { HeroImage } from "@/components/HeroImage";
 import { FeatherIcon } from "@/components/Main/NewestPost/FeatherIcon";
 import { FrontMatterLoading } from "@/components/Skeletons/ui/ContentLoading";
 import { RootLayout } from "@/features/RootLayout";
@@ -16,6 +15,8 @@ import {
 const NewestPost = dynamic(() => import("@/components/Main/NewestPost"), {
 	loading: () => <FrontMatterLoading length={2} />,
 });
+
+const HeroImage = dynamic(() => import("@/components/HeroImage/HeroImage"));
 
 export default async function Page() {
 	const savedTheme = (await cookies()).get("color-theme")?.value || "light";
