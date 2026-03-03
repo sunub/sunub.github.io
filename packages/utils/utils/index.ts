9022297692bb3f34f57f6c8e5e5489bcec4c6fc0
sunub <bsc5672@gmail.com>
2026-03-03 14:53:22 +0900

    refactor(shared) : Biome Formatting 적용

--- packages/utils/utils/index.ts
+++ packages/utils/utils/index.ts
@@ -1,7 +1,7 @@
-import { isIterable } from "./isIterable";
-import { toAsync } from "./toAsync";
 import { fromAsync } from "./fromAsync";
 import { isAsyncIterable } from "./isAsyncIterable";
+import { isIterable } from "./isIterable";
 import { isPromise } from "./isPromise";
+import { toAsync } from "./toAsync";
 
 export { isIterable, toAsync, fromAsync, isAsyncIterable, isPromise };
