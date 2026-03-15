6c1841c45c0fb2dd831b81cd9bd6c380b8a27322
sunub <bsc5672@gmail.com>
2026-03-16 01:54:58 +0900

    feat : 상수로 이미지 url을 추출

---
+++ frontend/src/resources/images.ts
@@ -0,0 +1,11 @@
+export const LIGHT_CRICAL_IMAGE_URLS = [
+	"/assets/light_clouds.webp",
+	"/assets/light_bridge.webp",
+	"/assets/light_cars.webp",
+];
+
+export const DARK_CRICAL_IMAGE_URLS = [
+	"/assets/dark_clouds.webp",
+	"/assets/dark_bridge.webp",
+	"/assets/dark_cars.webp",
+];
