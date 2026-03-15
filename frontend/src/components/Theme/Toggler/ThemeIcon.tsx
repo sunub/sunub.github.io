5c140e9e8891196414568e31ce894555dec45ddb
sunub <bsc5672@gmail.com>
2026-03-16 01:53:49 +0900

    refactor(frontend/theme-toggler): 테마 토글러의 역할을 상태 전환 중심으로 단순화

--- frontend/src/components/Theme/Toggler/ThemeIcon.tsx
+++ frontend/src/components/Theme/Toggler/ThemeIcon.tsx
@@ -1,73 +1,73 @@
 import { memo } from "react";
-import { SunAndBeams, Sun, Moon, SunAndMoon } from "./ThemeToggler.style";
+import { Moon, Sun, SunAndBeams, SunAndMoon } from "./ThemeToggler.style";
 
 export const ThemeIcon = memo(
-  ({
-    colorTheme,
-    maskId,
-    ...delegated
-  }: {
-    colorTheme: string;
-    maskId: string;
-  }) => {
-    const isDarkMode = colorTheme === "dark";
+	({
+		colorTheme,
+		maskId,
+		...delegated
+	}: {
+		colorTheme: string;
+		maskId: string;
+	}) => {
+		const isDarkMode = colorTheme === "dark";
 
-    return (
-      <SunAndMoon
-        {...delegated}
-        $primary={isDarkMode}
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <Sun
-          $primary={isDarkMode}
-          cx="12"
-          cy="12"
-          r="5"
-          fill="#2D0D06"
-          mask={`url(#${maskId})`}
-        />
-        <SunAndBeams $primary={isDarkMode}>
-          <path d="M12 3V3.52941" strokeWidth="3" strokeLinecap="round" />
-          <path
-            d="M5.63604 5.63604L6.01039 6.01039"
-            strokeWidth="3"
-            strokeLinecap="round"
-          />
-          <path d="M3 12L3.52941 12" strokeWidth="3" strokeLinecap="round" />
-          <path
-            d="M5.63604 18.364L6.01039 17.9896"
-            strokeWidth="3"
-            strokeLinecap="round"
-          />
-          <path d="M12 20.4706V21" strokeWidth="3" strokeLinecap="round" />
-          <path
-            d="M17.9896 17.9896L18.364 18.364"
-            strokeWidth="3"
-            strokeLinecap="round"
-          />
-          <path d="M20.4706 12L21 12" strokeWidth="3" strokeLinecap="round" />
-          <path
-            d="M17.9896 6.01039L18.364 5.63604"
-            strokeWidth="3"
-            strokeLinecap="round"
-          />
-        </SunAndBeams>
-        <Moon
-          id={maskId}
-          $primary={isDarkMode}
-          maskUnits={"userSpaceOnUse"}
-          maskContentUnits={"userSpaceOnUse"}
-        >
-          <rect x="0" y="0" width="100%" height="100%" fill="white" />
-          <circle cx="24" cy="24" r="6" fill="black" />
-        </Moon>
-      </SunAndMoon>
-    );
-  },
+		return (
+			<SunAndMoon
+				{...delegated}
+				$primary={isDarkMode}
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<Sun
+					$primary={isDarkMode}
+					cx="12"
+					cy="12"
+					r="5"
+					fill="#2D0D06"
+					mask={`url(#${maskId})`}
+				/>
+				<SunAndBeams $primary={isDarkMode}>
+					<path d="M12 3V3.52941" strokeWidth="3" strokeLinecap="round" />
+					<path
+						d="M5.63604 5.63604L6.01039 6.01039"
+						strokeWidth="3"
+						strokeLinecap="round"
+					/>
+					<path d="M3 12L3.52941 12" strokeWidth="3" strokeLinecap="round" />
+					<path
+						d="M5.63604 18.364L6.01039 17.9896"
+						strokeWidth="3"
+						strokeLinecap="round"
+					/>
+					<path d="M12 20.4706V21" strokeWidth="3" strokeLinecap="round" />
+					<path
+						d="M17.9896 17.9896L18.364 18.364"
+						strokeWidth="3"
+						strokeLinecap="round"
+					/>
+					<path d="M20.4706 12L21 12" strokeWidth="3" strokeLinecap="round" />
+					<path
+						d="M17.9896 6.01039L18.364 5.63604"
+						strokeWidth="3"
+						strokeLinecap="round"
+					/>
+				</SunAndBeams>
+				<Moon
+					id={maskId}
+					$primary={isDarkMode}
+					maskUnits={"userSpaceOnUse"}
+					maskContentUnits={"userSpaceOnUse"}
+				>
+					<rect x="0" y="0" width="100%" height="100%" fill="white" />
+					<circle cx="24" cy="24" r="6" fill="black" />
+				</Moon>
+			</SunAndMoon>
+		);
+	},
 );
 
 ThemeIcon.displayName = "ThemeIcon";
--- frontend/src/components/Theme/Toggler/ThemeToggler.tsx
+++ frontend/src/components/Theme/Toggler/ThemeToggler.tsx
@@ -1,37 +1,37 @@
 "use client";
 
-import { VisuallyHidden } from "@/components/VisuallyHidden";
-import { ToggleBtn } from "./ThemeToggler.style";
-import { ThemeIcon } from "./ThemeIcon.tsx";
-import { useTheme } from "@/components/Theme/ThemeProvider";
 import { useCallback } from "react";
 import { getNextTheme } from "@/components/HeroImage/heroImageResources";
+import { useTheme } from "@/components/Theme/ThemeProvider";
+import { VisuallyHidden } from "@/components/VisuallyHidden";
+import { ThemeIcon } from "./ThemeIcon.tsx";
+import { ToggleBtn } from "./ThemeToggler.style";
 
 export function ThemeToggler({
-  maskId,
-  "data-testid": dataTestId = "theme-toggler-button",
-  ...delegated
+	maskId,
+	"data-testid": dataTestId = "theme-toggler-button",
+	...delegated
 }: {
-  maskId: string;
-  "data-testid"?: string;
+	maskId: string;
+	"data-testid"?: string;
 }) {
-  const { colorTheme, setColorTheme } = useTheme();
+	const { colorTheme, setColorTheme } = useTheme();
 
-  const handleClick = useCallback(() => {
-    const nextTheme = getNextTheme(colorTheme);
-    setColorTheme(nextTheme);
-  }, [colorTheme, setColorTheme]);
+	const handleClick = useCallback(() => {
+		const nextTheme = getNextTheme(colorTheme);
+		setColorTheme(nextTheme);
+	}, [colorTheme, setColorTheme]);
 
-  return (
-    <ToggleBtn
-      {...delegated}
-      title="테마 변경"
-      aria-label="theme-toggler-button"
-      data-testid={dataTestId}
-      onClick={handleClick}
-    >
-      <VisuallyHidden>테마 변경 버튼</VisuallyHidden>
-      <ThemeIcon colorTheme={colorTheme} maskId={maskId} />
-    </ToggleBtn>
-  );
+	return (
+		<ToggleBtn
+			{...delegated}
+			title="테마 변경"
+			aria-label="theme-toggler-button"
+			data-testid={dataTestId}
+			onClick={handleClick}
+		>
+			<VisuallyHidden>테마 변경 버튼</VisuallyHidden>
+			<ThemeIcon colorTheme={colorTheme} maskId={maskId} />
+		</ToggleBtn>
+	);
 }
