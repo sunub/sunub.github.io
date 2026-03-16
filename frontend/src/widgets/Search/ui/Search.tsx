739f447018f0db057a38307504708da851f04900
sunub <bsc5672@gmail.com>
2026-03-16 14:30:18 +0900

    refactor : 불필요한 파일 제거 및 biome formatter 적용

--- frontend/src/widgets/Search/ui/Search.tsx
+++ frontend/src/widgets/Search/ui/Search.tsx
@@ -7,24 +7,24 @@ import { SearchButton } from "./SearchButton";
 import SearchModal from "./SearchModal";
 
 function Search() {
-  const { isOpen, open, close } = useSearchModal();
-  const portalRef = useRef<HTMLDivElement | null>(null);
+	const { isOpen, open, close } = useSearchModal();
+	const portalRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    if (!portalRef.current) {
-      portalRef.current = document.getElementById(
-        "blog-search__input-area",
-      ) as HTMLDivElement;
-    }
-  }, []);
+	useEffect(() => {
+		if (!portalRef.current) {
+			portalRef.current = document.getElementById(
+				"blog-search__input-area",
+			) as HTMLDivElement;
+		}
+	}, []);
 
-  return (
-    <>
-      <SearchButton isOpen={isOpen} toggleOpen={open} />
-      {isOpen &&
-        portalRef.current &&
-        createPortal(<SearchModal close={close} />, portalRef.current)}
-    </>
-  );
+	return (
+		<>
+			<SearchButton isOpen={isOpen} toggleOpen={open} />
+			{isOpen &&
+				portalRef.current &&
+				createPortal(<SearchModal close={close} />, portalRef.current)}
+		</>
+	);
 }
 export { Search };
