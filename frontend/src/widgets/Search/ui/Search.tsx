2fca3a18b9e2daa966c058ac6baf3f01e30a61aa
sunub <bsc5672@gmail.com>
2026-03-16 14:18:48 +0900

    refactor(search) : 불필요하게 동기적으로 로드하여 발생하는 fallback 제거

--- frontend/src/widgets/Search/ui/Search.tsx
+++ frontend/src/widgets/Search/ui/Search.tsx
@@ -1,35 +1,30 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 import { useSearchModal } from "../hook/useSearchAtoms";
 import { SearchButton } from "./SearchButton";
-
-const SearchModal = dynamic(() => import("./SearchModal"), {
-	ssr: false,
-	loading: () => <p>Loading...</p>,
-});
+import SearchModal from "./SearchModal";
 
 function Search() {
-	const { isOpen, open, close } = useSearchModal();
-	const portalRef = useRef<HTMLDivElement | null>(null);
+  const { isOpen, open, close } = useSearchModal();
+  const portalRef = useRef<HTMLDivElement | null>(null);
 
-	useEffect(() => {
-		if (!portalRef.current) {
-			portalRef.current = document.getElementById(
-				"blog-search__input-area",
-			) as HTMLDivElement;
-		}
-	}, []);
+  useEffect(() => {
+    if (!portalRef.current) {
+      portalRef.current = document.getElementById(
+        "blog-search__input-area",
+      ) as HTMLDivElement;
+    }
+  }, []);
 
-	return (
-		<>
-			<SearchButton isOpen={isOpen} toggleOpen={open} />
-			{isOpen &&
-				portalRef.current &&
-				createPortal(<SearchModal close={close} />, portalRef.current)}
-		</>
-	);
+  return (
+    <>
+      <SearchButton isOpen={isOpen} toggleOpen={open} />
+      {isOpen &&
+        portalRef.current &&
+        createPortal(<SearchModal close={close} />, portalRef.current)}
+    </>
+  );
 }
 export { Search };
